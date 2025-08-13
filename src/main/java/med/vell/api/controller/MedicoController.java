8df53fcfa4960012a7ba5f45873baba496cb0c93
EARNascimento <eduardo90909@outlook.com>
2025-08-13 15:39:51 -0300

    Criando um método para devolver informações após um 'GET', criando uma classe DadosListagemMedico para enviar as informações selecionadas

--- src/main/java/med/vell/api/controller/MedicoController.java
+++ src/main/java/med/vell/api/controller/MedicoController.java
@@ -3,13 +3,13 @@ package med.vell.api.controller;
 import jakarta.validation.Valid;
 import med.vell.api.endereco.Endereco;
 import med.vell.api.medico.DadosCadastroMedico;
+import med.vell.api.medico.DadosListagemMedico;
 import med.vell.api.medico.Medico;
 import med.vell.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/medicos")
@@ -21,5 +21,10 @@ public class MedicoController {
     @PostMapping
     public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados){
         repository.save(new Medico(dados));
+    }
+
+    @GetMapping
+    public List<DadosListagemMedico> lista(){
+        return repository.findAll().stream().map(DadosListagemMedico::new).toList();
     }
 }
