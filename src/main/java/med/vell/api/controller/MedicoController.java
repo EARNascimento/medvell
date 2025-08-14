d8dd8fc6e41c2aa6002ab0b481fe6c65eaac087b
EARNascimento <eduardo90909@outlook.com>
2025-08-14 09:49:31 -0300

    Adicionando o retorno ResponseEntity no método de deletar

--- src/main/java/med/vell/api/controller/MedicoController.java
+++ src/main/java/med/vell/api/controller/MedicoController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -39,8 +40,10 @@ public class MedicoController {
 
     @DeleteMapping("/{id}")
     @Transactional
-    public void deletar(@PathVariable Long id){
+    public ResponseEntity deletar(@PathVariable Long id){
         var medico = repository.getReferenceById(id);
         medico.excluir();
+
+        return ResponseEntity.noContent().build();
     }
 }
