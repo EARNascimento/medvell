a9ecc0a03f0754d38f5a47d9e558485970d88667
EARNascimento <eduardo90909@outlook.com>
2025-08-15 09:44:12 -0300

    Criando a insterface UsuarioRepository

---
+++ src/main/java/med/vell/api/domain/usuario/UsuarioRepository.java
@@ -0,0 +1,7 @@
+package med.vell.api.domain.usuario;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+}
